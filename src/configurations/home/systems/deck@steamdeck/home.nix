3750a73039c9e43d9834698ae1546187d168be1f
Jacob Ranson <code@jacobranson.dev>
2023-03-07 18:36:32 -0500

    Update home.nix

--- src/configurations/home/systems/deck@steamdeck/home.nix
+++ src/configurations/home/systems/deck@steamdeck/home.nix
@@ -3,4 +3,5 @@ inputs@{ config, pkgs, lib ? pkgs.lib, ... }:
 (lib.recursiveUpdate (import ../../home.nix inputs) {
   home.username = "deck";
   home.homeDirectory = "/home/deck";
+  xsession.enable = true; # allows for DE integration
 })
