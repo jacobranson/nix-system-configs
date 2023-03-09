7c8c76a6fcec28ac4f26ddb6f53638ff121487a6
Jacob Ranson <code@jacobranson.dev>
2023-03-08 21:05:11 -0500

    Update default.nix

--- src/configurations/home/default.nix
+++ src/configurations/home/default.nix
@@ -1,7 +1,7 @@
 inputs@{ pkgs, lib ? pkgs.lib, home-manager, ... }:
 
 {
-  "deck@steamdeck" = import ./systems/deck-steamdeck {
+  deck = import ./systems/deck-steamdeck {
     inherit (inputs) pkgs lib home-manager;
   };
 
